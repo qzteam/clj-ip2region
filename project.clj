861e783abbb52a385c458358c072e13b148dfe03
olOwOlo <olowolo233@gmail.com>
2019-12-23 12:37:47 +0800

    [lein release] prepare for next development iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-ip2region "1.0.1"
+(defproject clj-ip2region "1.0.2-SNAPSHOT"
   :description "ip2region clojure wrapper"
   :url "https://github.com/qzteam/clj-ip2region"
   :license {:name "EPL-2.0"
